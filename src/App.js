a135b7f394e76b54e0a58dd99feb71400ab53219
rahkned1990 <rahk1990@gmail.com>
2022-08-23 15:21:20 -0400

    Added child to parent prop in AddEntryForm to App

--- src/App.js
+++ src/App.js
@@ -18,12 +18,19 @@ function App() {
   // Props allow you to pass data to components
 
   // Anytime you want JS you have to use {} in html
-  const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}]); 
+  const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}, {weight: 176, date: '11-24-2021'}]); 
+
+  function addNewEntry(entry){
+
+    let tempEntries = [...entries, entry,]; 
+
+    setEntries(tempEntries); 
+  }
   return (
     <div >
       
       <DisplayEntries parentEntries={entries}/>
-      <AddEntryForm/>
+      <AddEntryForm addNewEntryProperty={addNewEntry}/>
   
     </div>
   );
