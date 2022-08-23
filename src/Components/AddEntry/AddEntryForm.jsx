67c0b68089edf6f4b945d168220135369b5aa6d8
rahkned1990 <rahk1990@gmail.com>
2022-08-23 14:08:29 -0400

    Added AddEntry folder & AddEntryForm component

---
+++ src/Components/AddEntry/AddEntryForm.jsx
@@ -0,0 +1,12 @@
+const AddEntryForm = (props) => {
+    return ( 
+        <form>
+            <label>Weight</label>
+            <input type='number'/>
+            <label>Date</label>
+            <input type='date'/>
+        </form>
+     );
+}
+ 
+export default AddEntryForm;
