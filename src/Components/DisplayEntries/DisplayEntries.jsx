cc34a18b004072f85e86619e0ea265a43eb2c5c3
rahkned1990 <rahk1990@gmail.com>
2022-08-23 13:04:56 -0400

    Added components folder and new file displayentrie

---
+++ src/Components/DisplayEntries/DisplayEntries.jsx
@@ -0,0 +1,35 @@
+// sfc gen component (shorthand)
+// name component the same as file
+
+const DisplayEntries = (props) => {
+    return (  
+        <table>
+        <thead>
+          <tr>
+            <th>Entry Number</th>
+            <th>Weight</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* THis line of code is not dynamic enough, use map instead
+           <tr>
+            <td>1</td>
+            <td>175</td>
+            <td>11-23-2021</td>
+          </tr> */}
+          {entries.map((entry ,index) => {
+            return (
+              <tr>
+                <td>{index + 1}</td>
+                <td>{entry.weight}</td>
+                <td>{entry.date}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    );
+}
+ 
+export default DisplayEntries;
